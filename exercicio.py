f1612d94e37b083e88d04af68079a81c366fbab3
marcalxx2024-code <marcalxx2024@gmail.com>
2026-06-22 23:31:38 -0300

    Create exercicio.py

---
+++ exercicio.py
@@ -0,0 +1,9 @@
+#---aprendendo lógica---3
+nome = "guilherme"
+idade = 25
+#---começando---#
+if idade >=18;
+  print(f"ola {nome}, você é maior de idade.')
+    else:
+      print("você é menor.")
+      #--fim--#
