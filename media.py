dfb9b35974631fee7a4df6b7603073aaee000619
marcalxx2024-code <marcalxx2024@gmail.com>
2026-06-24 16:18:39 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,7 @@
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: ")) 
+media = (nota1 + nota2) / 2
+if media >= 7:
+    print("O aluno foi aprovado!")
+else:
+    print("O aluno foi reprovado!") 
